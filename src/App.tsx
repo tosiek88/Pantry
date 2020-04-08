5caf59dc4e70a43c2f25622d638e0bcfc3f43bcf
Mateusz Tocha <tocha.mateusz@gmail.com>
2020-04-08 20:08:35 +0200

    Simple them start

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React from "react";
-import "./App.css";
+import { Toolbar } from '@material-ui/core/';
+import AppBar from '@material-ui/core/AppBar';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import React from 'react';
+import { Icon } from 'react-icons-kit';
+import { archive } from 'react-icons-kit/fa/archive';
+import './App.scss';
+import { useStyles } from './helpers/style';
 
-function App() {
-    return <div className="App">Test</div>;
-}
+const StoreIcon = () => <Icon icon={archive} size={40} />;
+
+const App = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={0}>
+        <Grid item xs={12} md={1}></Grid>
+        <Grid item xs={12} md={4}>
+          <div className={classes.banner}>
+            <h1>Your Pantry Store</h1>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={7}>
+          <Paper className={classes.content} elevation={10}>
+            <AppBar position="static">
+              <Toolbar variant="dense">
+                <StoreIcon />
+              </Toolbar>
+            </AppBar>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 export default App;
---
+++ src/helpers/style.ts
@@ -0,0 +1,56 @@
+import { createMuiTheme, makeStyles, Theme } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+
+export const theme: Theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#622012',
+        },
+        secondary: {
+            main: '#72A4C4',
+        },
+        contrastThreshold: 3,
+        tonalOffset: 0.2,
+    },
+});
+export const StyledButton = withStyles({
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    },
+    label: {
+        textTransform: 'capitalize',
+    },
+})(Button);
+
+export const useStyles = makeStyles((theme) => {
+    return {
+        root: {
+            flexGrow: 1,
+        },
+        banner: {
+            textAlign: 'center',
+            fontSize: '3vh',
+            fontFamily: 'Roboto, sans-serif',
+            textTransform: 'uppercase',
+            textShadow: '0px 10px 15px rgba(0, 0, 0, 1)',
+            [theme.breakpoints.up('sm')]: {
+                textAlign: 'right',
+                fontSize: '5vh',
+            },
+        },
+        content: {
+            backgroundColor: theme.palette.grey['A100'],
+            width: '400px',
+            height: '500px',
+            marginLeft: '50px',
+        },
+        navigation: {},
+    };
+});
