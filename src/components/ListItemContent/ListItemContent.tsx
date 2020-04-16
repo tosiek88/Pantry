689b2fd177eb347d3a50b56b0afe78ef03e52ef6
Mateusz Tocha <tocha.mateusz@gmail.com>
2020-04-16 19:21:30 +0200

    Cosmetics changes

--- src/components/ListItemContent/ListItemContent.tsx
+++ src/components/ListItemContent/ListItemContent.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import ListItemText from '@material-ui/core/ListItemText';
 import { IProduct } from 'store/store';
+import { upperCaseFirstLetter } from 'components/StoreListItem/StoreListItem';
 
 interface ProductContentProp {
     product: IProduct;
 }
 
-const upperCaseFirstLetter = (text: string): string => {
-    return text.charAt(0).toUpperCase() + text.substring(1);
-};
-
 export const ProductContent: React.FC<ProductContentProp> = ({ product }: ProductContentProp) => {
     return (
         <div style={{ width: '50%', marginLeft: '15px', marginRight: '15px' }}>
-            <div>{product.qty}</div>
             <div style={{ margin: '0 auto', width: '180px' }}>
                 <ListItemText
-                    primary={product.name}
+                    primary={upperCaseFirstLetter(product.name)}
                     secondary={`Minimum amount: ${product.minQty} [${product.unit}]`}
                 />
                 <p>
